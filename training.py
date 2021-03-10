a51e351e43ced1a076fc997a8c04892a096ea12b
roger <r.almato@opportunitynetwork.com>
2021-03-10 20:12:16 +0100

    fixed issue training

--- training.py
+++ training.py
@@ -27,7 +27,7 @@ REAL_DATASET_PATH = "dataset_real"
 now = datetime.now()
 results_folder_name = f"RESULTS/{now.day}-{now.month}-{now.year}_{now.hour}:{now.minute}:{now.second}"
 os.mkdir(str(os.getcwd()) + '/' + results_folder_name)
-
+os.mkdir(str(os.getcwd()) + '/' + results_folder_name + '/checkpoints')
 
 # ------------------------------------------------
 # -------------------DATASET----------------------
@@ -36,7 +36,7 @@ os.mkdir(str(os.getcwd()) + '/' + results_folder_name)
 dataset_obj = Dataset(CARTOON_DATASET_PATH, REAL_DATASET_PATH, batch_size)
 dataset = dataset_obj.dataset_numpy
 
-
+print(dataset.shape)
 # ------------------------------------------------
 # -----------------CALLBACKS----------------------
 # ------------------------------------------------
@@ -65,27 +65,14 @@ class DisplayImages(tf.keras.callbacks.Callback):
 
     plt.savefig(results_folder_name + '/' + 'image_at_epoch_{:04d}.png'.format(epoch))
 
-    if epoch % 50 == 0:
-        n = len(dataset)
-        random = randrange(n)
-        batch = dataset[random]
-        result = xgan.generator(batch, is_cartoon = False, training=False)
-        cartoon = result['image_a']
-        fig = plt.figure(figsize=(6,12))
-    # real images
-    for i in range(16):
-        fig.add_subplot(8, 4, i+1)
-        img = batch[i-16, :, :, :] * 127.5 + 127.5
-        plt.imshow(img.astype("uint8"))
-        plt.axis('off')
-    # cartoon images predicted
-    for i in range(16, 32):
-        fig.add_subplot(8, 4, i+1)
-        img_prediction = cartoon[i-16, :, :, :] * 127.5 + 127.5
-        plt.imshow(img_prediction.numpy().astype("uint8"))
-        plt.axis('off')
 
-    plt.savefig(results_folder_name + '/' + 'CHECK_{:04d}.png'.format(epoch))
+checkpoint_path = f"{results_folder_name}/checkpoints/"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
 
 # ------------------------------------------------
 # --------------------TRAIN-----------------------
@@ -98,4 +85,4 @@ xgan.compile(
     loss=tf.keras.losses.BinaryCrossentropy(from_logits=True)
 )
 
-xgan.fit(dataset, epochs=num_epochs, callbacks=[DisplayImages()])
+xgan.fit(dataset, epochs=num_epochs, callbacks=[DisplayImages(), cp_callback])
